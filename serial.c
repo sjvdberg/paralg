b9e30bac2b1d2027324f932e9561ee0c290176c6
Sander <sander.vd.berg@kpnmail.nl>
2023-11-09 12:42:03 +0100

    setup test

--- serial.c
+++ serial.c
@@ -2,13 +2,20 @@
 #include <stdbool.h>
 #include <math.h>
 
-
 void main()
 {
+    sieve(1000)
+    sieve(10000)
+    sieve(100000)
+    sieve(1000000)
+}
 
-    long n = 1000000;
+void sieve(long n)
+{
 
-    bool values[1000000] = {false}; 
+    bool values[n]; 
+    for(int i = 0; i < n; i++)
+        values[i] = false;
     int strikecounter, ifcounter;
     strikecounter = 0; 
     ifcounter = 0;
@@ -17,16 +24,16 @@ void main()
         ifcounter += 1;
         if(values[i] != false)
             continue;
-        printf("%d \n", i);
         ifcounter += 1;
         if(i*i >= n) 
             continue;
-        for(long long int j = i*i; j < n; j+=i)
+        for(long long int j = i; j < n; j+=i)
             {
             strikecounter += 1;
             values[j] = true;
             }
     }
+    printtf("regular. n = %d.\n", n);
     printf("strike counter is %d \n", strikecounter);
     printf("if counter is %d \n", ifcounter);
     getchar();
