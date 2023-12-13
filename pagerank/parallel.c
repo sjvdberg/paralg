b2cb3a0efb8108f1106ec883ac91a96739fef295
Sander <sander.vd.berg@kpnmail.nl>
2023-12-13 01:00:25 +0100

    started parallel program

--- pagerank/parallel.c
+++ pagerank/parallel.c
@@ -1,3 +1,83 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+    int p, s;
+
+    /* SPMD part */
+    MPI_Init(&argc,&argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD,&p); /* p = number of processors */ 
+    MPI_Comm_rank(MPI_COMM_WORLD,&s); /* s = processor number */ 
+
+    int n=1000;
+    if (argc>1) n=atoi(argv[1]);
+
+    if(n<0) MPI_Abort(MPI_COMM_WORLD,-1);
+
+
+
+    MPI_Finalize();
+    return 0;
+} /* end main */
+
+//n is number of rows/columns.
+//p is number of processors.
+//s is own rank.
+void computeVector(int N, int p, int s, MPI_Comm comm)
+{
+
+    int numrows = (N+p-s-1)/p ;
+    int remainder = N % p;
+    int firstrow;
+    if( s < remainder) 
+        //numrows is the same as all earlier processors.
+        firstrow = s * numrows;
+    else
+        //The first remainder processors have 1 more element
+        firstrow = s * numrows + remainder;
+    
+    int baseRows[numrows][11];
+    for(int i = 0; i < numrows; i++)
+    {
+        int k = rand() % 10;
+        for(int l = 0; l < 11; l++)
+        {
+            if(l <= k)
+                baseRows[i][l] = rand() % N;
+            else
+                baseRows[i][l] = -1;
+        }
+    }
+
+    int localDiagonal[N];
+    for(int i = 0; i < N; i++)
+        localDiagonal[i] = 0;
+    for(int i = 0; i < numrows; i++)
+        for(int l = 0; l < 10; l++)
+            if(baseRows[i][l] != -1)
+                localDiagonal[baseRows[i][l]]++;
+    int Diagonals[N][p];
+    for(int r = 0; r < p; r++)
+    {
+        if(r == s)
+        {
+            for(int i = 0; i < numrows; i++)
+                Diagonals[r][i] = localDiagonal[i];
+            continue;
+        }
+        MPI_Isend(localDiagonal, N, MPI_INT, r, r, comm);
+    }
+    MPI_Barrier(comm);
+    for(int r = 0; r < p; r++)
+    {
+        if(r == s) continue;
+        MPI_Irecv(localDiagonal, N, MPI_INT, r, s, comm);
+        for(int i = 0; i < numrows; i++)
+            Diagonals[r][i] = localDiagonal[i];
+    }
+    printf("succesfully computed diagonal.\n");
+}
