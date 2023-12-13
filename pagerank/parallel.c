235239e510916088deb5cf36dcf3c0826b6e52ec
Sander <sander.vd.berg@kpnmail.nl>
2023-12-13 14:23:12 +0100

    changed random seed

--- pagerank/parallel.c
+++ pagerank/parallel.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 //s is own rank.
 void computeVector(int N, int p, int s, MPI_Comm comm)
 {
-    srand(s);
+    srand(time(0));
     int numrows = (N+p-s-1)/p ;
     int remainder = N % p;
     int firstrow;
