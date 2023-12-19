bc77ae28c145867ff83c2a7780e425355a7c2f11
Sander <sander.vd.berg@kpnmail.nl>
2023-12-19 17:13:21 +0100

    changed typo

--- test/test.c
+++ test/test.c
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
         MPI_Send(arr, 5, MPI_INT, 1, 1, MPI_COMM_WORLD);
         printf("Message Sent:\n");
         for(int i = 0; i < 5; i++)
-            printf("%i. %i\n", s, i);
+            printf("%i. %i\n", process_Rank, i);
     }
 
     else if(process_Rank == 1){
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
         MPI_Recv(receive, 5, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Message Received:\n");
         for(int i = 0; i < 5; i++)
-            printf("%i. %i\n", s, receive[i]);
+            printf("%i. %i\n", process_Rank, receive[i]);
     }
 
     MPI_Finalize();
