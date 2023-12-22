2edbd2b8c452e3780625edc26a073740d24dce03
Sander <sander.vd.berg@kpnmail.nl>
2023-12-22 15:54:54 +0100

    made send nonblockign

--- pagerank/parallel.c
+++ pagerank/parallel.c
@@ -195,7 +195,7 @@ void computeVector(long N, int p, int s, MPI_Comm comm)
     for(long r = 0; r < p; r++)
     {
         if(r != s)
-            MPI_Send(&tot, 1, MPI_INT, r, r, comm, &req[r]);
+            MPI_Isend(&tot, 1, MPI_INT, r, r, comm, &req[r]);
     }
     MPI_Barrier(comm);
     MPI_Wait(req);
