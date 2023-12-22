f9b2fd9b0aea6510012e41283b721175751d2dfd
Sander <sander.vd.berg@kpnmail.nl>
2023-12-22 15:15:15 +0100

    added print

--- pagerank/parallel.c
+++ pagerank/parallel.c
@@ -180,7 +180,9 @@ void computeVector(long N, int p, int s, MPI_Comm comm)
     float u[numrows], res[numrows], tempr[N];
     long tot = 0;
     
-
+    for(long i = 0; i < numElements; i++)
+        if(rows[i] > N)
+            printf("%i. Rows[%ld] value is %ld\n", s, i, rows[i]);
     for(long i = 0; i < numrows; i++)
     {
         long k = rand() % (N*1000);
