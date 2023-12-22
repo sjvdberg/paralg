f9d8fbec668a4fc15fa7ee9f74c292ad6ddbf562
Sander <sander.vd.berg@kpnmail.nl>
2023-12-22 15:16:02 +0100

    changed print location

--- pagerank/parallel.c
+++ pagerank/parallel.c
@@ -180,9 +180,7 @@ void computeVector(long N, int p, int s, MPI_Comm comm)
     float u[numrows], res[numrows], tempr[N];
     long tot = 0;
     
-    for(long i = 0; i < numElements; i++)
-        if(rows[i] > N)
-            printf("%i. Rows[%ld] value is %ld\n", s, i, rows[i]);
+    
     for(long i = 0; i < numrows; i++)
     {
         long k = rand() % (N*1000);
@@ -215,6 +213,9 @@ void computeVector(long N, int p, int s, MPI_Comm comm)
         norms[i] = -1;
     float prob = 0.85;
 
+    for(long i = 0; i < numElements; i++)
+        if(rows[i] > N)
+            printf("%i. Rows[%ld] value is %ld\n", s, i, rows[i]);
     
     for(long i = 0; i < numrows; i++)
     {
